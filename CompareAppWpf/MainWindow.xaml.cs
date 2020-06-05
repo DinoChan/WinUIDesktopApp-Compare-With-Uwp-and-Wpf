9c923bc27caa72fc137352d9f334160d0b82a576
Dino Chan <dino.chan@live.com>
2020-06-05 15:19:26 +0800

    Create Wpf Project

--- CompareAppWpf/MainWindow.xaml.cs
+++ CompareAppWpf/MainWindow.xaml.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace CompareAppWpf
@@ -22,7 +13,41 @@ namespace CompareAppWpf
     {
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                var rectangle = new Rectangle
+                {
+                    Height = 500,
+                    Width = 500,
+                    Opacity = ((double)i + 40) / 100d,
+                    RadiusX = 108,
+                    RadiusY = 98,
+                    StrokeThickness = 3,
+                    Stroke = new SolidColorBrush(Color.FromArgb(255, 75, 75, (byte)(i * 250d / 50d))),
+                    RenderTransformOrigin = new Point(0.5, 0.5)
+                };
+                Root.Children.Add(rectangle);
+                var angle = i * 360d / 50d;
+                var transform = new RotateTransform
+                {
+                    Angle = angle
+                };
+
+                rectangle.RenderTransform = transform;
+
+                var storyboard = new Storyboard();
+                var animation = new DoubleAnimation { Duration = TimeSpan.FromSeconds(1), From = angle, To = angle + 360 };
+                storyboard.Children.Add(animation);
+                Storyboard.SetTarget(animation, rectangle);
+                Storyboard.SetTargetProperty(animation, new PropertyPath("RenderTransform.Angle"));
+                storyboard.RepeatBehavior = RepeatBehavior.Forever;
+                storyboard.Begin();
+            }
         }
     }
 }
